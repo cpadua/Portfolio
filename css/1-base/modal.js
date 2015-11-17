e2215cba35b8cd6a60a98169c416841b16135fbf
Cristian Padua <paduac@ist2.net>
2015-11-17 11:26:32 -0500

    added more modals for myworks

--- css/1-base/modal.js
+++ css/1-base/modal.js
@@ -15,3 +15,39 @@ $(function() {
     e.stopPropagation();
   });
 });
+
+$(function() {
+  $("#modal-2").on("change", function() {
+    if ($(this).is(":checked")) {
+      $("body").addClass("modal-open");
+    } else {
+      $("body").removeClass("modal-open");
+    }
+  });
+
+  $(".modal-fade-screen, .modal-close").on("click", function() {
+    $(".modal-state:checked").prop("checked", false).change();
+  });
+
+  $(".modal-inner").on("click", function(e) {
+    e.stopPropagation();
+  });
+});
+
+$(function() {
+  $("#modal-3").on("change", function() {
+    if ($(this).is(":checked")) {
+      $("body").addClass("modal-open");
+    } else {
+      $("body").removeClass("modal-open");
+    }
+  });
+
+  $(".modal-fade-screen, .modal-close").on("click", function() {
+    $(".modal-state:checked").prop("checked", false).change();
+  });
+
+  $(".modal-inner").on("click", function(e) {
+    e.stopPropagation();
+  });
+});
